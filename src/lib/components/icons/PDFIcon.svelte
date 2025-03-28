2fbe20815726d13a5801a14e42e425346ea55341
Katowicer <katowicer0@proton.me>
2025-03-28 20:21:02 +0100

    feat(ui): added icon

---
+++ src/lib/components/icons/PDFIcon.svelte
@@ -0,0 +1,11 @@
+<svg
+	xmlns="http://www.w3.org/2000/svg"
+	class="h-8 w-8 text-red-500"
+	viewBox="0 0 20 20"
+	fill="currentColor"
+>
+	<path
+		d="M6 2a2 2 0 00-2 2v12a2 2 0 002 2h8a2 2 0 002-2V7.414A2 2 0 0015.414 6L11 1.586A2 2 0 009.586 1H6z"
+	/>
+	<path d="M8 9a1 1 0 012 0v3a1 1 0 11-2 0V9z" />
+</svg>
