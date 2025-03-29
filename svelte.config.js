a671cca090f270875209f3cc436194a578e3140c
Katowicer <katowicer0@proton.me>
2025-03-29 16:48:59 +0100

    ci(deploy): fixed deployment

--- svelte.config.js
+++ svelte.config.js
@@ -1,21 +1,16 @@
 import adapter from '@sveltejs/adapter-static';
-import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 
-/** @type {import('@sveltejs/kit').Config} */
-const config = {
-	preprocess: vitePreprocess(),
-	kit: {
-		adapter: adapter({
-			fallback: '404.html'
-		}),
-		paths: {
-			base: process.argv.includes('dev') ? '' : process.env.BASE_PATH
-		}
-		prerender: {
-			entries: ['*']
-		}
-	}
+const dev = process.env.NODE_ENV === 'development';
+
+export default {
+  kit: {
+    adapter: adapter(),
+    paths: {
+      base: dev ? '' : 'AFIN'
+    },
+    prerender: {
+      entries: ['*']
+    }
+  }
 };
-
-export default config;
 
