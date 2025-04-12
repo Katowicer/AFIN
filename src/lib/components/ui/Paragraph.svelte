40cb42ef6377c26cfcbb442b6ed024da1b30798a
Katowicer <katowicer0@proton.me>
2025-04-12 20:46:59 +0200

    feat(ui): + XLSX icon

--- src/lib/components/ui/Paragraph.svelte
+++ src/lib/components/ui/Paragraph.svelte
@@ -3,7 +3,7 @@
 	let { children } = $props<{ children: Snippet }>();
 </script>
 
-<div class="px-2 border-solid border-l-black hover:border-l-2 hover:pb-5 transition-all duration-500">
+<div class="px-2 hover:pb-5 transition-all duration-500">
 	<p class="mb-4 text-sm sm:text-base md:text-lg">
 		{@render children()}
 	</p>
